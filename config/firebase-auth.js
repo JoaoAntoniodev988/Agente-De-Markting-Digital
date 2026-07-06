8b895556dde81b742622a79a7d95765e1d1b6e08
JoaoAntoniodev988 <joaodesousaantonio2@gmail.com>
2026-07-06 01:30:30 +0100

    e

--- config/firebase-auth.js
+++ config/firebase-auth.js
@@ -3,50 +3,23 @@ import app from "./firebase-config.js";
 import {
   getAuth,
   FacebookAuthProvider,
-  signInWithPopup,
-  onAuthStateChanged,
-  signOut
+  signInWithPopup
 } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
 
 const auth = getAuth(app);
-
 const provider = new FacebookAuthProvider();
 
-/**
- * Login com Facebook
- */
 export async function loginFacebook() {
   try {
-
     const result = await signInWithPopup(auth, provider);
 
-    console.log("Login realizado!");
+    console.log("Login efetuado com sucesso!");
+    console.log(result.user);
 
     return result.user;
 
   } catch (error) {
-
-    console.error(error);
-
+    console.error("Erro no login:", error);
     throw error;
-
   }
-}
-
-/**
- * Logout
- */
-export async function logoutFacebook() {
-
-  await signOut(auth);
-
-}
-
-/**
- * Observa alterações de sessão
- */
-export function observarAutenticacao(callback){
-
-    onAuthStateChanged(auth, callback);
-
 }
